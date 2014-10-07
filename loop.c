9eae96e6dc74a873f40a2c2c4e9cded44ec77f70
Rob Bassi <rob.bassi.89@gmail.com>
2014-10-07 14:49:27 -0400

    Added function to display directory contents

--- loop.c
+++ loop.c
@@ -1,19 +1,36 @@
-// linked from tree.asm
+#define DT_DIR		4
 void puts(char*, int);
 
 struct linux_dirent {
   unsigned long  d_ino;     /* Inode number */
-  unsigned long  d_off;     /* Offset to next linux_dirent */
+  unsigned long  d_off;     /* Offset to next linux_dirent  */
   unsigned short d_reclen;  /* Length of this linux_dirent */
   char           d_name[];  /* Filename (null-terminated) */  
+  /* char           d_type; */
 };
+
+void display_contents(struct linux_dirent entries[]) {
+  while (entries->d_ino > 0) {
+    if (is_directory(entries))
+      puts("(directory) ", 11);
+    println(entries->d_name);
+    entries = (struct linux_dirent *) (((char *) entries) + entries->d_reclen);
+  }
+}
+
+int is_directory(struct linux_dirent *entry) {
+  return *(((char*) entry) + (entry->d_reclen - 1)) == DT_DIR;
+}
 
 void print_strs(int l, char**strs) {
   int i;
-  for (i = 0; i < l; i++) {
-    puts(strs[i] , len(strs[i]));
-    puts("\n", 1);
-  }
+  for (i = 0; i < l; i++)
+    println(strs[i]);
+}
+
+void println(char *str) {
+  puts(str, len(str));
+  puts("\n", 1);
 }
 
 int len (char *s) {
